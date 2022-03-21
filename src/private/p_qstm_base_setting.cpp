bcce68a0c3dab48e09326b9eea87b126f5a3076f
Magma-Flavio Portela <fmspx@hotmail.com>
2022-03-21 20:32:49 -0300

    refactor

--- src/private/p_qstm_base_setting.cpp
+++ src/private/p_qstm_base_setting.cpp
@@ -1,11 +1,12 @@
 #include "./p_qstm_base_setting.h"
 
 namespace QStm {
-namespace Private{
+namespace Private {
+
+typedef QStandardPaths StdPaths;
 
 Q_GLOBAL_STATIC(QVariantHash, __static_dirs)
 Q_GLOBAL_STATIC(QVariantHash, __static_variables)
-
 
 QVariantHash &static_dirs()
 {
@@ -14,73 +15,73 @@ QVariantHash &static_dirs()
 
 QVariantHash &make_static_variables()
 {
-    auto &dir=*__static_dirs;
+    auto &d = *__static_dirs;
 
 #ifndef Q_OS_IOS
     QProcess process;
     auto lst = process.environment();
-    for(auto&v:lst){
-        auto s=v.split(qsl("="));
-        auto env=s.first().trimmed();
-        auto value=s.last().trimmed();
-        dir.insert(env, value);
+    for (auto &v : lst) {
+        auto s = v.split(qsl("="));
+        auto env = s.first().trimmed();
+        auto value = s.last().trimmed();
+        d[env] = value;
     }
 #endif
-    dir.insert(qsl("$DesktopLocation"      ).trimmed(), QStandardPaths::writableLocation(QStandardPaths::DesktopLocation      ) );
-    dir.insert(qsl("$DocumentsLocation"    ).trimmed(), QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation    ) );
-    dir.insert(qsl("$FontsLocation"        ).trimmed(), QStandardPaths::writableLocation(QStandardPaths::FontsLocation        ) );
-    dir.insert(qsl("$ApplicationsLocation" ).trimmed(), QStandardPaths::writableLocation(QStandardPaths::ApplicationsLocation ) );
-    dir.insert(qsl("$MusicLocation"        ).trimmed(), QStandardPaths::writableLocation(QStandardPaths::MusicLocation        ) );
-    dir.insert(qsl("$MoviesLocation"       ).trimmed(), QStandardPaths::writableLocation(QStandardPaths::MoviesLocation       ) );
-    dir.insert(qsl("$PicturesLocation"     ).trimmed(), QStandardPaths::writableLocation(QStandardPaths::PicturesLocation     ) );
-    dir.insert(qsl("$TempLocation"         ).trimmed(), QStandardPaths::writableLocation(QStandardPaths::TempLocation         ) );
-    dir.insert(qsl("$HomeLocation"         ).trimmed(), QStandardPaths::writableLocation(QStandardPaths::HomeLocation         ) );
+    d[qsl("$DesktopLocation")] = StdPaths::writableLocation(StdPaths::DesktopLocation);
+    d[qsl("$DocumentsLocation")] = StdPaths::writableLocation(StdPaths::DocumentsLocation);
+    d[qsl("$FontsLocation")] = StdPaths::writableLocation(StdPaths::FontsLocation);
+    d[qsl("$ApplicationsLocation")] = StdPaths::writableLocation(StdPaths::ApplicationsLocation);
+    d[qsl("$MusicLocation")] = StdPaths::writableLocation(StdPaths::MusicLocation);
+    d[qsl("$MoviesLocation")] = StdPaths::writableLocation(StdPaths::MoviesLocation);
+    d[qsl("$PicturesLocation")] = StdPaths::writableLocation(StdPaths::PicturesLocation);
+    d[qsl("$TempLocation")] = StdPaths::writableLocation(StdPaths::TempLocation);
+    d[qsl("$HomeLocation")] = StdPaths::writableLocation(StdPaths::HomeLocation);
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    dir.insert(qsl("$DataLocation"         ).trimmed(), QStandardPaths::writableLocation(QStandardPaths::DataLocation         ) );
+    d[qsl("$DataLocation")] = StdPaths::writableLocation(StdPaths::DataLocation);
 #endif
-    dir.insert(qsl("$CacheLocation"        ).trimmed(), QStandardPaths::writableLocation(QStandardPaths::CacheLocation        ) );
-    dir.insert(qsl("$GenericDataLocation"  ).trimmed(), QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation  ) );
-    dir.insert(qsl("$RuntimeLocation"      ).trimmed(), QStandardPaths::writableLocation(QStandardPaths::RuntimeLocation      ) );
-    dir.insert(qsl("$ConfigLocation"       ).trimmed(), QStandardPaths::writableLocation(QStandardPaths::ConfigLocation       ) );
-    dir.insert(qsl("$DownloadLocation"     ).trimmed(), QStandardPaths::writableLocation(QStandardPaths::DownloadLocation     ) );
-    dir.insert(qsl("$GenericCacheLocation" ).trimmed(), QStandardPaths::writableLocation(QStandardPaths::GenericCacheLocation ) );
-    dir.insert(qsl("$GenericConfigLocation").trimmed(), QStandardPaths::writableLocation(QStandardPaths::GenericConfigLocation) );
-    dir.insert(qsl("$AppDataLocation"      ).trimmed(), QStandardPaths::writableLocation(QStandardPaths::AppDataLocation      ) );
-    dir.insert(qsl("$AppConfigLocation"    ).trimmed(), QStandardPaths::writableLocation(QStandardPaths::AppConfigLocation    ) );
-    dir.insert(qsl("$AppLocalDataLocation" ).trimmed(), QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation ) );
+    d[qsl("$CacheLocation")] = StdPaths::writableLocation(StdPaths::CacheLocation);
+    d[qsl("$GenericDataLocation")] = StdPaths::writableLocation(StdPaths::GenericDataLocation);
+    d[qsl("$RuntimeLocation")] = StdPaths::writableLocation(StdPaths::RuntimeLocation);
+    d[qsl("$ConfigLocation")] = StdPaths::writableLocation(StdPaths::ConfigLocation);
+    d[qsl("$DownloadLocation")] = StdPaths::writableLocation(StdPaths::DownloadLocation);
+    d[qsl("$GenericCacheLocation")] = StdPaths::writableLocation(StdPaths::GenericCacheLocation);
+    d[qsl("$GenericConfigLocation")] = StdPaths::writableLocation(StdPaths::GenericConfigLocation);
+    d[qsl("$AppDataLocation")] = StdPaths::writableLocation(StdPaths::AppDataLocation);
+    d[qsl("$AppConfigLocation")] = StdPaths::writableLocation(StdPaths::AppConfigLocation);
+    d[qsl("$AppLocalDataLocation")] = StdPaths::writableLocation(StdPaths::AppLocalDataLocation);
+    d[qsl("$HOME")] = StdPaths::writableLocation(StdPaths::HomeLocation);
+    d[qsl("$TEMPDIR")] = StdPaths::writableLocation(StdPaths::TempLocation);
+    d[qsl("$APPDIR")] = StdPaths::writableLocation(StdPaths::ApplicationsLocation);
+    d[qsl("$APPNAME")] = qApp->applicationName();
 
-    dir.insert(qsl("$HOME"),QStandardPaths::writableLocation(QStandardPaths::HomeLocation));
-    dir.insert(qsl("$TEMPDIR"),QStandardPaths::writableLocation(QStandardPaths::TempLocation));
-    dir.insert(qsl("$APPDIR"),QStandardPaths::writableLocation(QStandardPaths::ApplicationsLocation));
-    dir.insert(qsl("$APPNAME"),qApp->applicationName());
-
-    return dir;
+    return d;
 }
 
 void set_static_variables(const QVariantHash &v)
 {
-    *__static_variables=v;
+    *__static_variables = v;
     QHashIterator<QString, QVariant> i(*__static_dirs);
-    while (i.hasNext()){
+    while (i.hasNext()) {
         i.next();
-        __static_variables->insert(i.key(), i.value());
+        auto s = i.value().toString().trimmed();
+        if (s.isEmpty())
+            continue;
+        (*__static_variables)[i.key()]=s;
     }
 }
 
 void init()
 {
-    *__static_variables=QStm::Private::make_static_variables();
+    *__static_variables = QStm::Private::make_static_variables();
 }
 
 Q_COREAPP_STARTUP_FUNCTION(init)
 
 QVariantHash &static_variables()
 {
-    return*__static_variables;
+    return *__static_variables;
 }
 
-}
+} // namespace Private
 
-
-}
-
+} // namespace QStm
